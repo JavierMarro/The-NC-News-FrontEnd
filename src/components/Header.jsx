32a8370ed9b1b898a366ddead1c1b12b21ced622
Javier <javier.martinezrom@gmail.com>
2024-12-17 15:28:02 +0100

    Link added to header directing to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-      <h1>The NC News</h1>
+      <Link to="/home" style={{ textDecoration: "none" }}>
+        <h1>The NC News</h1>
+      </Link>
     </header>
   );
 };
