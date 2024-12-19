2ee0095a205e9d59351290130d69d86e34794433
Javier <javier.martinezrom@gmail.com>
2024-12-19 15:03:27 +0100

    Login button changed and footer added + refactoring

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="footer">
+        © 2024 The NC News. Made by Javier Martínez Romera.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/NewComment.jsx
@@ -0,0 +1,66 @@
+import { Form, InputGroup, Button, Row, Col, Container } from "react-bootstrap";
+import { useState, useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { postComment } from "../api";
+
+const NewComment = ({ article_id, handleCommentPosted }) => {
+  const [newComment, setNewComment] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const {
+    user: { username },
+  } = useContext(UserContext);
+
+  const handleChange = (e) => {
+    setNewComment(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setIsError(false);
+    postComment(article_id, username, newComment)
+      .then(() => {
+        setNewComment("");
+        setIsLoading(false);
+        handleCommentPosted();
+      })
+      .catch((error) => {
+        setIsError(true);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} className="mb-3 mt-3">
+      <Row className="mb-3 mt-3">
+        <Col>
+          <InputGroup>
+            <Form.Control
+              as="textarea"
+              aria-label="With textarea"
+              onChange={handleChange}
+              name="new-comment"
+              value={newComment}
+              type="text"
+              required
+              placeholder="Type your comment here..."
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+      <Container className="mb-3">
+        <Button variant="dark" type="submit" disabled={isLoading}>
+          {isLoading ? "Submitting..." : "Submit"}
+        </Button>
+      </Container>
+      {isError && (
+        <p style={{ color: "red" }}>
+          Failed to post comment. Please try again.
+        </p>
+      )}
+    </Form>
+  );
+};
+
+export default NewComment;
